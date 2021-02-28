9ba2328ebbad9ca20f668f9f187515795c822a2b
Derek Drennan <derekay@live.com>
2021-02-28 11:27:00 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -28,9 +28,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {this.state.monsters.map(monster => 
-          <h1 key={ monster.id }> { monster.name } </h1>
-        )}
+        {this.state.monsters.map(monster => <h1 key={ monster.id }> { monster.name } </h1>)}
       </div>
     );
   }
